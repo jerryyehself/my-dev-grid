37f3fee40b4e35be8a270a11fff5da60946a8557
jerryyehself <jerry40522@gmail.com>
2025-07-16 00:20:43 +0800

    fix(ui): 更新vue根組件

--- resources/js/pages/App.vue
+++ resources/js/pages/App.vue
@@ -1,8 +1,41 @@
 <template>
-    <AppHeader />
-    <router-view />
+    <header
+        class="h-20 bg-gradient-to-b from-stone-900 to-stone-700 text-stone-200 grid grid-cols-4 items-center p-2 box-border shadow-black shadow-sm relative z-10"
+    >
+        <h1
+            class="col-span-1 xl:text-4xl md:text-2xl font-bold relative inline-block font-serif"
+        >
+            <a href="/" class="block p-3">My Dev Grid</a>
+        </h1>
+        <nav
+            class="col-span-2 flex space-x-20 text-stone-300 font-medium justify-center xl:text-xl md:text-md"
+        >
+            <RouterLink
+                v-for="(page, key) in pages"
+                :key="key"
+                :to="page.to"
+                class="relative inline-block px-1 after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full hover:text-white"
+            >
+                {{ page.label }}
+            </RouterLink>
+        </nav>
+    </header>
+    <RouterView />
 </template>
 
 <script setup>
-import AppHeader from "../components/layouts/AppHeader.vue";
+const pages = {
+    home: {
+        to: "/",
+        label: "Home",
+    },
+    article: {
+        to: "/triple-control",
+        label: "Admin",
+    },
+    about: {
+        to: "/projects",
+        label: "Projects",
+    },
+};
 </script>
