3dc5e6d7c3491c25db02aa92b1c5041b2a70c55c
jerryyehself <jerry40522@gmail.com>
2025-11-21 21:42:31 +0800

    (feature)add Logo font

--- resources/js/pages/AppAboutMe.vue
+++ resources/js/pages/AppAboutMe.vue
@@ -1,19 +1,53 @@
 <template>
-    <div class="h-full px-20 py-5 grid grid-cols-[1fr_2fr]">
-        <div class="col-span-1 flex flex-col">
-            <img
-                src="https://avatars.githubusercontent.com/u/87906523?v=4"
-                alt=""
-                class=""
-            />
-            <h2 class="text-2xl font-bold mt-4 text-center">Jerry Yeh</h2>
-            <p class="text-stone-600 mt-2 text-center">
-                A passionate developer with a love for creating innovative web
-                applications. Skilled in Vue.js, Laravel, and modern web
-                technologies.
-            </p>
+    <div class="h-full px-20 pb-16 pt-8">
+        <div
+            class="grid grid-cols-[1fr_3fr] h-full shadow-xl border-1 border-stone-200"
+        >
+            <div
+                class="col-span-1 flex flex-col px-5 py-10 bg-stone-300 items-center gap-6"
+            >
+                <img
+                    src="https://avatars.githubusercontent.com/u/87906523?v=4"
+                    alt=""
+                    class="rounded-full h-50 w-50"
+                />
+                <h2 class="text-2xl font-bold text-center">Jerry Yeh</h2>
+
+                <ul class="text-stone-600 text-left gap-1 flex flex-col flex-1">
+                    <li>
+                        <a
+                            href="mailto:jerry40522@gmail.com"
+                            class="inline-flex items-center gap-1"
+                        >
+                            <EnvelopeIcon
+                                class="h-[1em] w-[1em] flex-shrink-0"
+                            />
+                            jerry40522@gmail.com</a
+                        >
+                    </li>
+                    <li>
+                        <a href="https://github.com/jerryyehself"
+                            >jerryyehself</a
+                        >
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/jerryyeh/"
+                            >LinkedIn</a
+                        >
+                    </li>
+                    <li>
+                        <a href="https://jerryyehself.github.io/"
+                            >Personal Blog</a
+                        >
+                    </li>
+                </ul>
+            </div>
+            <!-- <div class="w-1 mx-4 my-auto h-1/2 bg-stone-400"></div> -->
+            <div></div>
         </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { EnvelopeIcon } from "@heroicons/vue/16/solid";
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,10 @@
 <head>
     <meta charset="UTF-8" />
     <title>{{ env('APP_NAME') }}</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Playwrite+NZ+Guides&family=Story+Script&family=Merriweather:wght@400;700&family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
